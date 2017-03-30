4dfe853f56b554e9f6b7dcabe2f7b3558396953c
Joel Forster <newlocaluser@imac9.complex.ucl.ac.uk>
2017-03-30 18:06:29 +0100

    algorithm works, added new graph types, shakespeare quote

--- morannode.py
+++ morannode.py
@@ -1,9 +1,22 @@
 import random
 from deap import creator, base, tools, algorithms
 import numpy
+import string
+from difflib import SequenceMatcher
+
+
 
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMax,nFitness=0)
+
+legalChars = string.ascii_letters+" '.?,"
+
+def similar(a, b):
+	f = 1
+	for i in range(len(a)):
+		d = abs(legalChars.index(a[i]) - legalChars.index(b[i]))
+		f += min(d,len(legalChars)-d)
+	return 1.0/f
 
 class MoranNode:
 	def __init__(self,popsize):
@@ -12,12 +25,15 @@ class MoranNode:
 		self.toolbox = base.Toolbox()
 
 		self.toolbox.register("attr_bool", random.randint, 0, 1)
-		self.toolbox.register("individual", tools.initRepeat, creator.Individual, self.toolbox.attr_bool, n=100)
+		self.toolbox.register("attr_letter",random.choice,legalChars)
+
+
+		self.toolbox.register("individual", tools.initRepeat, creator.Individual, self.toolbox.attr_letter, n=39)
 		self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
 
-		self.toolbox.register("evaluate", self.evalOneMax)
+		self.toolbox.register("evaluate", self.evalString)
 		self.toolbox.register("mate", tools.cxTwoPoint)
-		self.toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+		self.toolbox.register("mutate", self.mutateChar, indpb=0.05)
 		self.toolbox.register("select", tools.selTournament, tournsize=3)
 
 		self.population = self.toolbox.population(n=popsize)
@@ -29,6 +45,20 @@ class MoranNode:
 		stats.register("std", numpy.std)
 		stats.register("min", numpy.min)
 		stats.register("max", numpy.max)
+
+	def mutateChar(self,individual,indpb):
+		for i in xrange(len(individual)):
+			if random.random() < indpb:
+				c = individual[i]
+				index = legalChars.index(c)
+				nindex = random.randint(index-1,index+1)
+				if nindex >= len(legalChars):
+					nindex = nindex - len(legalChars)
+				individual[i] = legalChars[nindex]
+		return individual,
+
+	def evalString(self,individual,target="Shall I compare thee to a summer's day?"):
+		return similar(''.join(individual),target),
 
 	def evalOneMax(self,individual):
 		return sum(individual),
--- network.py
+++ network.py
@@ -5,14 +5,39 @@ import matplotlib.pyplot as plt
 import random
 import morannode as mn
 
-def createStar(n):
+def createBiStar(n,p=10):
 	G = nx.DiGraph()
-	G.add_node(0,mNode=mn.MoranNode(10))
+	G.add_node(0,mNode=mn.MoranNode(p))
 	G.add_edge(0,0)
-	edges = []
 	for i in range(1,n+1):
-		G.add_node(i,mNode=mn.MoranNode(10))
+		G.add_node(i,mNode=mn.MoranNode(p))
 		G.add_edge(i,0)
+		G.add_edge(0,i)
+	return G
+
+def createStar(n,p=10):
+	G = nx.DiGraph()
+	G.add_node(0,mNode=mn.MoranNode(p))
+	G.add_edge(0,0)
+	for i in range(1,n+1):
+		G.add_node(i,mNode=mn.MoranNode(p))
+		G.add_edge(i,0)
+	return G
+
+def createIslands(n,p=10):
+	G = nx.DiGraph()
+	for i in range(n):
+		G.add_node(i,mNode=mn.MoranNode(p))
+	for i in range(n):
+		for j in range(n):
+			if i!=j:
+				G.add_edge(i,j)
+	return G
+
+def createSinglets(n,p=10):
+	G = nx.DiGraph()
+	for i in range(n):
+		G.add_node(i,mNode=mn.MoranNode(p))
 	return G
 
 class MoranGraph:
@@ -43,42 +68,96 @@ class MoranGraph:
 			for f in n[-1]['mNode'].population:
 				t+=f.nFitness
 				if(t>s):
+					if (len(self.graph.edges(n[0]))<1):
+						#print('node has no edges')
+						return f
 					destInd = random.choice(self.graph.edges(n[0]))
 					if (destInd[-1] == n[0]):
 						#print('node has no outgoing edges')
 						return f
 					dest = self.graph.node[destInd[-1]]
 					destM = dest['mNode']
-					toReplace = random.randint(0,len(destM.population)-1)
+					toReplace = self.getLeastFitFromNode(destInd[-1])[-1]
+					#print('before' + str( destM.population[toReplace].fitness.values[0]))
 					destM.population[toReplace] = f
+					#print('after' + str( destM.population[toReplace].fitness.values[0]))
 					#print('moving ' + str(f) + ' to ' + str(destInd[-1]) + ' from ' +  str(n[0]))
 					return (f)
 
 	def getFittestFromNode(self,n):
 		mFit = -1
 		mInd = -1
+		fittest = []
 		for i in range(len(self.graph.node[n]['mNode'].population)):
 			f = self.graph.node[n]['mNode'].population[i]
 			if (f.fitness.values[0]>mFit):
 				mFit = f.fitness.values[0]
 				mInd = i
+				fittest = f
 
-		return (mFit,mInd)
+		return (mFit,fittest,mInd)
+
+	def getLeastFitFromNode(self,n):
+		mFit = -1
+		mInd = -1
+		fittest = []
+		for i in range(len(self.graph.node[n]['mNode'].population)):
+			f = self.graph.node[n]['mNode'].population[i]
+			if (f.fitness.values[0]<mFit):
+				mFit = f.fitness.values[0]
+				mInd = i
+				fittest = f
+
+		return (mFit,fittest,mInd)
+
+	def getFittest(self):
+		mFit = -1
+		nInd = -1
+		mInd = -1
+		for i in range(len(self.graph.node)):
+			f = self.getFittestFromNode(i)
+			if f[0]>mFit:
+				ind = f[1]
+				mFit = f[0]
+				mInd = f[-1]
+				nInd = i
+
+		return (nInd,mFit,mInd,''.join(ind))
 
 
 def main():
 
-	G = MoranGraph(createStar(3))
+	islands = MoranGraph(createIslands(10,10))
+	star = MoranGraph(createStar(9,10))
+	bistar = MoranGraph(createBiStar(9,10))
+	singlet = MoranGraph(createSinglets(10,10))
 
 
-	for gen in range(100):
-		G.evaluateNodes()
-		G.moranProcess()
-		print (G.getFittestFromNode(0)[0])
+	for gen in range(1000):
 
-	pos = nx.spring_layout(G.graph)
-	nx.draw_networkx(G.graph, pos, cmap=plt.get_cmap('jet'),with_labels = True, edgelist=G.graph.edges(), arrows=True)
-	plt.show()
+
+		islands.evaluateNodes()
+		islands.moranProcess()
+
+		star.evaluateNodes()
+		star.moranProcess()
+
+		bistar.evaluateNodes()
+		bistar.moranProcess()
+
+		singlet.evaluateNodes()
+		singlet.moranProcess()
+
+		print (str(islands.getFittest()[1])+","+str(star.getFittest()[1])+","+str(bistar.getFittest()[1])+","+str(singlet.getFittest()[1]))
+	
+	print ('islands --- ' + str(islands.getFittest()))
+	print ('star --- ' + str(star.getFittest()))
+	print ('bistar --- ' + str(bistar.getFittest()))
+	print ('singlet ---' + str(singlet.getFittest()))
+
+	#pos = nx.spring_layout(G.graph)
+	#nx.draw_networkx(G.graph, pos, cmap=plt.get_cmap('jet'),with_labels = True, edgelist=G.graph.edges(), arrows=True)
+	#plt.show()
 
 if __name__ == "__main__":
 	main()
